a1bcc08962b2915358349ce7959c23f7f9833975
Rishab Gupta <rishabgupta98@gmail.com>
2023-04-05 23:17:24 -0300

    adds cors

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 app.get("/api/todo", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    todos: [
+      {
+        id: 1,
+        title: "Todo One",
+      },
+      {
+        id: 2,
+        title: "Todo Two",
+      },
+      {
+        id: 3,
+        title: "Todo Three",
+      },
+    ],
+  });
 });
 
 app.listen(port, () => {
