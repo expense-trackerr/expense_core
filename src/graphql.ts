c615ecb5b7e5c1d15301781204f9960c6e29ba96
Rishab Gupta <rishabgupta98@gmail.com>
2023-06-24 17:50:41 -0400

    removes graphql

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,26 +1,43 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-// GraphQL schema
-const schema = buildSchema(`
-    type Query {
-        message: String
-    }
-`);
-// Root resolver
-const root = {
-  message: () => 'Hello World!',
-};
-// Create an express server and a GraphQL endpoint
-const app = express();
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
-app.listen(4000, () =>
-  console.log('Express GraphQL Server Now Running On localhost:4000/graphql')
-);
+// const express = require('express');
+// const { ApolloServer, gql } = require('apollo-server-express');
+// // import db from './db/database';
+// const db = require('../src/db/database');
+
+// // GraphQL schema
+// const schema = gql`
+//     type Query {
+//         categories(userUid: String!): {name: String}[]
+//     }
+// `;
+
+// // Root resolver
+// const resolvers = {
+//   categories: async ({ userUid }: { userUid: string }) => {
+//     try {
+//       if (!userUid) {
+//         throw new Error(
+//           'User ID is not present. Ensure that you are logged in to the application'
+//         );
+//       }
+//       const categories = await db.query(
+//         'SELECT name FROM categories WHERE user_uid = ?',
+//         [userUid]
+//       );
+//       return categories[0];
+//     } catch (error) {
+//       throw new Error((error as Error).message);
+//     }
+//   },
+// };
+
+// const server = new ApolloServer({
+//   schema,
+//   resolvers,
+// });
+
+// const app = express();
+// server.applyMiddleware({ app });
+
+// app.listen({ port: 4000 }, () =>
+//   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+// );
