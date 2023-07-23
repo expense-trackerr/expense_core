ae9524649fc29ec9f594578fab9a1bc1467c73f2
Rishab Gupta <rishabgupta98@gmail.com>
2023-07-23 12:36:09 -0400

    get transactions endpoint

--- src/routes/plaid.ts
+++ src/routes/plaid.ts
@@ -4,10 +4,21 @@ import { plaidClient } from '../config/plaid-config';
 import { UserInfoRequest } from '../utils/express-types';
 
 const router = express.Router();
-let ACCESS_TOKEN = '';
+let ACCESS_TOKEN = 'access-sandbox-565b0d29-b155-4bc7-b5fc-1275e050d721';
 const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || Products.Transactions).split(',') as Products[];
 const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(',') as CountryCode[];
 const PLAID_REDIRECT_URI = process.env.PLAID_REDIRECT_URI || '';
+
+const compareTxnsByDateAscending = (a: Transaction, b: Transaction) => {
+  const aDate = new Date(a.date).valueOf();
+  const bDate = new Date(b.date).valueOf();
+  if (aDate > bDate) {
+    return 1;
+  } else if (aDate < bDate) {
+    return -1;
+  }
+  return 0;
+};
 
 router.post('/create_link_token', async (request: UserInfoRequest, response, next) => {
   try {
@@ -50,51 +61,40 @@ router.post('/set_access_token', function (request, response, next) {
     .catch(next);
 });
 
-router.get('/transactions', function (request, response, next) {
-  Promise.resolve()
-    .then(async function () {
-      // Set cursor to empty to receive all historical updates
-      let cursor = undefined;
+router.get('/transactions', async (request, response, next) => {
+  try {
+    let cursor = undefined;
 
-      // New transaction updates since "cursor"
-      let added: Transaction[] = [];
-      let modified: Transaction[] = [];
-      // Removed transaction ids
-      let removed: RemovedTransaction[] = [];
-      let hasMore = true;
-      // Iterate through each page of new transaction updates for item
-      while (hasMore) {
-        const transactionsRequest: TransactionsSyncRequest = {
-          access_token: ACCESS_TOKEN,
-          cursor: cursor,
-        };
-        const transactions = await plaidClient.transactionsSync(transactionsRequest);
-        const data = transactions.data;
-        // Add this page of results
-        added = added.concat(data.added);
-        modified = modified.concat(data.modified);
-        removed = removed.concat(data.removed);
-        hasMore = data.has_more;
-        // Update cursor to the next cursor
-        cursor = data.next_cursor;
-        console.log(transactions.data);
-      }
-
-      const compareTxnsByDateAscending = (a: Transaction, b: Transaction) => {
-        const aDate = new Date(a.date).valueOf();
-        const bDate = new Date(b.date).valueOf();
-        if (aDate > bDate) {
-          return 1;
-        } else if (aDate < bDate) {
-          return -1;
-        }
-        return 0;
+    // New transaction updates since "cursor"
+    let added: Transaction[] = [];
+    let modified: Transaction[] = [];
+    // Removed transaction ids
+    let removed: RemovedTransaction[] = [];
+    let hasMore = true;
+    // Iterate through each page of new transaction updates for item
+    while (hasMore) {
+      const transactionsRequest: TransactionsSyncRequest = {
+        access_token: ACCESS_TOKEN,
+        cursor: cursor,
       };
+      const transactions = await plaidClient.transactionsSync(transactionsRequest);
+      console.log('transactions:', transactions);
+      const data = transactions.data;
+      // Add this page of results
+      added = added.concat(data.added);
+      modified = modified.concat(data.modified);
+      removed = removed.concat(data.removed);
+      hasMore = data.has_more;
+      // Update cursor to the next cursor
+      cursor = data.next_cursor;
+    }
 
-      const recentlyAdded = [...added].sort(compareTxnsByDateAscending);
-      response.json({ latest_transactions: recentlyAdded });
-    })
-    .catch(next);
+    const recentlyAdded = [...added].sort(compareTxnsByDateAscending);
+    response.json({ latest_transactions: recentlyAdded });
+  } catch (error) {
+    console.error('Error getting transactions:', error);
+    next();
+  }
 });
 
 module.exports = router;
