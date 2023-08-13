825e86f3798286ab8230027c93b657d50e44da90
Rishab Gupta <rishabgupta98@gmail.com>
2023-08-13 12:48:21 -0300

    creates a migration

--- src/prisma/migrations/20230813154630_creates_whole_schema/migration.sql
+++ src/prisma/migrations/20230813154630_creates_whole_schema/migration.sql
@@ -1,19 +1,14 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
 
-  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `user_id` on the `User` table. All the data in the column will be lost.
-  - Added the required column `id` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updated_at` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `User` DROP PRIMARY KEY,
-    DROP COLUMN `user_id`,
-    ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    ADD COLUMN `id` VARCHAR(191) NOT NULL,
-    ADD COLUMN `updated_at` DATETIME(3) NOT NULL,
-    ADD PRIMARY KEY (`id`);
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Category` (
