054471e23955003ab8a6796648f23c1e56197bc6
Rishab Gupta <rishabgupta98@gmail.com>
2023-09-05 00:07:44 -0300

    removes all migrations

--- src/prisma/migrations/20230820184043_change_case_category_type_table_enum/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [INCOME,EXPENSE] on the enum `CategoryType_name` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `CategoryType` MODIFY `name` ENUM('Income', 'Expense') NOT NULL;
