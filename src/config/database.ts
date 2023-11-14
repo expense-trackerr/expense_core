d6e449f5bc21260ce7c696450e08c45cb368b1f9
Rishab Gupta <rishabgupta98@gmail.com>
2023-11-14 18:10:28 -0400

    Adding temp

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,12 @@
-import mysql from 'mysql2';
 import { PrismaClient } from '@prisma/client';
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'mysqlpassword',
-  database: 'Expenses',
-});
+// const pool = mysql.createPool({
+//   host: 'expense-tracker.cj6jqm9d7pyp.us-east-2.rds.amazonaws.com',
+//   user: 'root',
+//   password: 'mysqlpassword',
+//   database: 'expense-tracker',
+// });
 
-export default pool.promise();
+// export default pool.promise();
 
 export const prisma = new PrismaClient();
