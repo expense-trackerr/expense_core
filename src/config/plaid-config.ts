e4661e220e7875d2e258339c4c5fbc0a30067dde
Rishab Gupta <rishabgupta98@gmail.com>
2023-12-09 18:20:52 -0400

    attempt at getting the secrets

--- src/config/plaid-config.ts
+++ src/config/plaid-config.ts
@@ -2,15 +2,58 @@ import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
 import { getSecrets } from './get-secrets';
 require('dotenv').config();
 
-const resolvedPlaidSecrets = getSecrets();
-console.log('resolvedPlaidSecrets:', resolvedPlaidSecrets);
+import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
+
+type PlaidSecrets = {
+  PLAID_CLIENT_ID: string;
+  PLAID_SECRET: string;
+  PLAID_ENV: string;
+  PLAID_PRODUCTS: string;
+  PLAID_COUNTRY_CODES: string;
+};
+
+let plaidSecrets: Record<string, string> = {};
+
+const getPlaidSecretFromAws = async () => {
+  const secretName = 'sandbox/plaid';
+
+  const client = new SecretsManagerClient({
+    region: 'us-east-2',
+  });
+
+  try {
+    const response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secretName,
+        VersionStage: 'AWSCURRENT', // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+
+    if (response.SecretString) {
+      return JSON.parse(response.SecretString) as PlaidSecrets;
+    }
+    return {};
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+};
+
+// wait for the getPlaidSecretFromAws to resolve before continuing
+
+getPlaidSecretFromAws().then((secrets) => {
+  plaidSecrets = secrets;
+  console.log('plaidSecrets:', plaidSecrets);
+});
+
+console.log('resolvedPlaidSecrets:', plaidSecrets);
 
 const configuration = new Configuration({
-  basePath: PlaidEnvironments[resolvedPlaidSecrets.PLAID_ENV],
+  basePath: PlaidEnvironments[plaidSecrets.PLAID_ENV],
   baseOptions: {
     headers: {
-      'PLAID-CLIENT-ID': resolvedPlaidSecrets.PLAID_CLIENT_ID,
-      'PLAID-SECRET': resolvedPlaidSecrets.PLAID_SECRET,
+      'PLAID-CLIENT-ID': plaidSecrets.PLAID_CLIENT_ID,
+      'PLAID-SECRET': plaidSecrets.PLAID_SECRET,
       'Plaid-Version': '2020-09-14',
     },
   },
